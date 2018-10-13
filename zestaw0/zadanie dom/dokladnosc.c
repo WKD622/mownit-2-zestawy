c318037a874f01cca0bfbd8d58a84b3513f37840
WKD622 <kuba.ziark@gmail.com>
2018-10-13 22:18:44 +0200

    added gsl ex

---
+++ zestaw0/zadanie dom/dokladnosc.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <gsl/gsl_ieee_utils.h>
+#include <gsl/gsl_interp.h>
+#define N 10
+
+int main(void)
+{
+  float f = 1.0 / 3.0;
+  double d = 1.0 / 3.0;
+
+  double fd = f; /* promote from float to double */
+
+  // printf(" f="); gsl_ieee_printf_float(&f);
+  // printf("\n");
+
+  // printf("fd="); gsl_ieee_printf_double(&fd);
+  // printf("\n");
+
+  int i;
+  for (i = 0; i < N; i++)
+  {
+    printf("%d\n d=", i+1);
+    gsl_ieee_printf_double(&d);
+    printf("\n");
+    printf(" d= %.50f\n", d);
+    d = d / 4.5;
+  }
+
+  return 0;
+}
